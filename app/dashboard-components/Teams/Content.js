832fee19d61bb0ef533eea2b3904f59d9fbcd753
Roukmaar <umaar.farouq123@gmail.com>
2025-11-18 18:58:52 +0100

    Add files via upload

--- app/dashboard-components/Teams/Content.js
+++ app/dashboard-components/Teams/Content.js
@@ -4,6 +4,11 @@ import { faPencilAlt } from "@fortawesome/free-solid-svg-icons";
 import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Content() {
   return (
@@ -19,7 +24,6 @@ export default function Content() {
             + Add Member
           </button>
         </div>
-
         <input
           type="text"
           placeholder="Search members..."
@@ -32,7 +36,10 @@ export default function Content() {
               <div className="flex items-center mb-4 justify-between">
                 <div className="flex space-x-4">
                   <div className="bg-blue-200 text-blue-950 rounded-full h-12 w-12 flex items-center justify-center font-semibold">
-                    AA
+                    <Image src="/alex.jpg" alt=""
+                      width={10000000}
+                      height={0}
+                      className="rounded-full h-12 w-12" />
                   </div>
                   <div>
                     <h2 className="font-bold text-black">Alexander Akosile O.</h2>
@@ -63,20 +70,32 @@ export default function Content() {
               </p>
               <hr className="my-4 border-gray-300" />
               <div className="text-sm text-gray-600 space-y-1">
-                <p className="flex gap-1.5 items-center">
-                  <FontAwesomeIcon
-                    icon={faEnvelope}
-                    className="size-4 text-blue-900"
-                  />{" "}
-                  alexakosile1@gmail.com
+                <p className="flex gap-1.5 items-center hover:underline">
+                  <a
+                    href="https://mail.google.com/mail/?view=cm&fs=1&to=alexakosile1@gmail.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex gap-1.5 items-center"
+                  >
+                    <FontAwesomeIcon icon={faEnvelope} className="size-4 text-blue-900" />
+                    alexakosile1@gmail.com
+                  </a>
                 </p>
                 <p className="flex gap-1.5 items-center">
                   <FontAwesomeIcon
-                    icon={faPhone}
+                    icon={faWhatsapp}
                     className="size-4 text-blue-900"
                   />{" "}
                   (+234) 80337 70253
                 </p>
+                <Link href="https://x.com/kaptal_alex?t=gFaKBZpDgfBcqc1F_ftw2A&s=08">
+                  <p className="flex gap-1.5 items-center hover:underline ">
+                    <FontAwesomeIcon
+                      icon={faTwitter}
+                      className="size-4 text-blue-900" />
+                    @kaptal_alex
+                  </p>
+                </Link>
               </div>
             </div>
 
@@ -84,7 +103,10 @@ export default function Content() {
               <div className="flex items-center mb-4 justify-between">
                 <div className="flex space-x-4">
                   <div className="bg-blue-200 text-blue-950 rounded-full h-12 w-12 flex items-center justify-center font-semibold">
-                    AP
+                    <Image src="/praise2.jpg" alt=""
+                      width={10000000}
+                      height={0}
+                      className="rounded-full h-12 w-12" />
                   </div>
                   <div>
                     <h2 className="font-bold text-black">Adeogun Praise E.</h2>
@@ -112,22 +134,34 @@ export default function Content() {
               </div>
               <p className="text-sm text-gray-600 mb-4">
                 Ensures data integrity, security, and performance.</p>
-                <hr className="my-4 border-gray-300" />
+              <hr className="my-4 border-gray-300" />
               <div className="text-sm text-gray-600 space-y-1">
-                <p className="flex gap-1.5 items-center">
-                  <FontAwesomeIcon
-                    icon={faEnvelope}
-                    className="size-4 text-blue-900"
-                  />{" "}
-                  spraiseadeogun2@gmail.com
+                <p className="flex gap-1.5 items-center hover:underline">
+                  <a
+                    href="https://mail.google.com/mail/?view=cm&fs=1&to=spraiseadeogun2@gmail.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex gap-1.5 items-center"
+                  >
+                    <FontAwesomeIcon icon={faEnvelope} className="size-4 text-blue-900" />
+                    spraiseadeogun2@gmail.com
+                  </a>
                 </p>
                 <p className="flex gap-1.5 items-center">
                   <FontAwesomeIcon
-                    icon={faPhone}
+                    icon={faWhatsapp}
                     className="size-4 text-blue-900"
                   />{" "}
                   (+234) 91617 84554
                 </p>
+                <Link href="https://x.com/kaptal_alex?t=gFaKBZpDgfBcqc1F_ftw2A&s=08">
+                  <p className="flex gap-1.5 items-center hover:underline ">
+                    <FontAwesomeIcon
+                      icon={faTwitter}
+                      className="size-4 text-blue-900" />
+                    @kaptal_alex
+                  </p>
+                </Link>
               </div>
             </div>
           </div>
@@ -137,7 +171,10 @@ export default function Content() {
               <div className="flex items-center mb-4 justify-between">
                 <div className="flex space-x-4">
                   <div className="bg-blue-200 text-blue-950 rounded-full h-12 w-12 flex items-center justify-center font-semibold">
-                    SO
+                    <Image src="/samuel.jpg" alt=""
+                      width={10000000}
+                      height={0}
+                      className="rounded-full h-12 w-12" />
                   </div>
                   <div>
                     <h2 className="font-bold text-black">Samuel Okpokam D.</h2>
@@ -168,20 +205,32 @@ export default function Content() {
               </p>
               <hr className="my-4 border-gray-300" />
               <div className="text-sm text-gray-600 space-y-1">
-                <p className="flex gap-1.5 items-center">
-                  <FontAwesomeIcon
-                    icon={faEnvelope}
-                    className="size-4 text-blue-900"
-                  />{" "}
-                  m.chen@example.com
+                <p className="flex gap-1.5 items-center hover:underline">
+                  <a
+                    href="https://mail.google.com/mail/?view=cm&fs=1&to=isaacnewtondewizard.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex gap-1.5 items-center"
+                  >
+                    <FontAwesomeIcon icon={faEnvelope} className="size-4 text-blue-900" />
+                    isaacnewtondewizard.com
+                  </a>
                 </p>
                 <p className="flex gap-1.5 items-center">
                   <FontAwesomeIcon
-                    icon={faPhone}
+                    icon={faWhatsapp}
                     className="size-4 text-blue-900"
                   />{" "}
                   (+234) 81003 67291
                 </p>
+                <Link href="https://x.com/isaac_newt27907?t=dVjor3SVbEU7sTU0Qy-v2w&s=08">
+                  <p className="flex gap-1.5 items-center hover:underline ">
+                    <FontAwesomeIcon
+                      icon={faTwitter}
+                      className="size-4 text-blue-900" />
+                    @isaac_newt27907
+                  </p>
+                </Link>
               </div>
             </div>
 
@@ -189,7 +238,10 @@ export default function Content() {
               <div className="flex items-center mb-4 justify-between">
                 <div className="flex space-x-4">
                   <div className="bg-blue-200 text-blue-950 rounded-full h-12 w-12 flex items-center justify-center font-semibold">
-                    OH
+                    <Image src="/haliyah.jpg" alt=""
+                      width={10000000}
+                      height={0}
+                      className="rounded-full h-12 w-12" />
                   </div>
                   <div>
                     <h2 className="font-bold text-black">Onatade Haliyah O.</h2>
@@ -220,79 +272,188 @@ export default function Content() {
               </p>
               <hr className="my-4 border-gray-300" />
               <div className="text-sm text-gray-600 space-y-1">
-                <p className="flex gap-1.5 items-center">
-                  <FontAwesomeIcon
-                    icon={faEnvelope}
-                    className="size-4 text-blue-900"
-                  />{" "}
+                <p className="flex gap-1.5 items-center hover:underline">
+                <a
+                  href="https://mail.google.com/mail/?view=cm&fs=1&to=opeyemihaliyah6@gmail.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex gap-1.5 items-center"
+                >
+                  <FontAwesomeIcon icon={faEnvelope} className="size-4 text-blue-900" />
                   opeyemihaliyah6@gmail.com
+                </a>
                 </p>
-                <p className="flex gap-1.5 items-center">
-                  <FontAwesomeIcon
-                    icon={faPhone}
-                    className="size-4 text-blue-900"
-                  />{" "}
-                  (+234) 91282 68359
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="grid lg:grid-cols-2">
-            <div className="bg-white p-4 rounded-xl shadow">
-              <div className="flex items-center mb-4 justify-between">
-                <div className="flex space-x-4">
-                  <div className="bg-blue-200 text-blue-950 rounded-full h-12 w-12 flex items-center justify-center font-semibold">
-                    MC
-                  </div>
-                  <div>
-                    <h2 className="font-bold text-black">Olatunde U.Farouq</h2>
-                    <p className="text-sm text-blue-600">Member</p>
-                  </div>
-                </div>
-                <div className="flex space-x-2 text-gray-500">
-                  <FontAwesomeIcon
-                    icon={faPencilAlt}
-                    className="size-4 text-blue-900 hover:text-blue-700 cursor-pointer"
-                  />
-                  <FontAwesomeIcon
-                    icon={faTrashAlt}
-                    className="size-4 text-red-600 hover:text-red-400 cursor-pointer"
-                  />
-                </div>
-              </div>
-              <div className="flex space-x-2 mb-2">
-                <span className="bg-blue-800 text-white text-xs px-2 py-0.5 rounded-full">
-                  active
-                </span>
-                <span className="bg-gray-200 text-gray-700 text-xs px-2 py-0.5 rounded-full">
-                  Frontend
-                </span>
-              </div>
-              <p className="text-sm text-gray-600 mb-4">
-                Creates fast, responsive, and engaging interfaces.
+              <p className="flex gap-1.5 items-center">
+                <FontAwesomeIcon
+                  icon={faWhatsapp}
+                  className="size-4 text-blue-900"
+                />{" "}
+                (+234) 91282 68359
               </p>
-              <hr className="my-4 border-gray-300" />
-              <div className="text-sm text-gray-600 space-y-1">
-                <p className="flex gap-1.5 items-center">
+              <Link href="https://x.com/yemmiemotara?t=oHuT5pl-ib9rnkr0RpvmEw&s=08">
+                <p className="flex gap-1.5 items-center hover:underline ">
                   <FontAwesomeIcon
-                    icon={faEnvelope}
-                    className="size-4 text-blue-900"
-                  />{" "}
-                  umaar.farouq123@gmail.com
+                    icon={faTwitter}
+                    className="size-4 text-blue-900" />
+                  @yemmiemotara
                 </p>
-                <p className="flex gap-1.5 items-center">
-                  <FontAwesomeIcon
-                    icon={faPhone}
-                    className="size-4 text-blue-900"
-                  />{" "}
-                  (+234) 70537 25664
-                </p>
-              </div>
+              </Link>
             </div>
           </div>
         </div>
+
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 w-full ">
+          <div className="bg-white p-4 rounded-xl shadow">
+            <div className="flex items-center mb-4 justify-between">
+              <div className="flex space-x-4">
+                <div className="bg-blue-200 text-blue-950 rounded-full h-12 w-12 flex items-center justify-center font-semibold">
+                  <Image src="/farouq.jpg" alt=""
+                    width={10000000}
+                    height={0}
+                    className="rounded-full h-12 w-12" />
+                </div>
+                <div>
+                  <h2 className="font-bold text-black">Olatunde U.Farouq</h2>
+                  <p className="text-sm text-blue-600">Member</p>
+                </div>
+              </div>
+              <div className="flex space-x-2 text-gray-500">
+                <FontAwesomeIcon
+                  icon={faPencilAlt}
+                  className="size-4 text-blue-900 hover:text-blue-700 cursor-pointer"
+                />
+                <FontAwesomeIcon
+                  icon={faTrashAlt}
+                  className="size-4 text-red-600 hover:text-red-400 cursor-pointer"
+                />
+              </div>
+            </div>
+            <div className="flex space-x-2 mb-2">
+              <span className="bg-blue-800 text-white text-xs px-2 py-0.5 rounded-full">
+                active
+              </span>
+              <span className="bg-gray-200 text-gray-700 text-xs px-2 py-0.5 rounded-full">
+                Frontend
+              </span>
+            </div>
+            <p className="text-sm text-gray-600 mb-4">
+              Creates fast, responsive, and engaging interfaces.
+            </p>
+            <hr className="my-4 border-gray-300" />
+            <div className="text-sm text-gray-600 space-y-1">
+                <p className="flex gap-1.5 items-center hover:underline">
+                <a
+                  href="https://mail.google.com/mail/?view=cm&fs=1&to=umaar.farouq123@gmail.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex gap-1.5 items-center"
+                >
+                  <FontAwesomeIcon icon={faEnvelope} className="size-4 text-blue-900" />
+                  umaar.farouq123@gmail.com
+                </a>
+              </p>
+              <p className="flex gap-1.5 items-center">
+                <FontAwesomeIcon
+                  icon={faWhatsapp}
+                  className="size-4 text-blue-900"
+                />{" "}
+                (+234) 70537 25664
+              </p>
+              <Link href="https://x.com/Umarfarouq8080?s=08">
+                <p className="flex gap-1.5 items-center hover:underline ">
+                  <FontAwesomeIcon
+                    icon={faTwitter}
+                    className="size-4 text-blue-900" />
+                  @Umarfarouq8080
+                </p>
+              </Link>
+            </div>
+          </div>
+
+
+          <div className="bg-white p-4 rounded-xl shadow">
+            <div className="flex items-center mb-4 justify-between">
+              <div className="flex space-x-4">
+                <div className="bg-blue-200 text-blue-950 rounded-full h-12 w-12 flex items-center justify-center font-semibold">
+                  <Image src="/praise2.jpg" alt=""
+                    width={10000000}
+                    height={0}
+                    className="rounded-full h-12 w-12" />
+                </div>
+                <div>
+                  <h2 className="font-bold text-black">Ajibade Oluwaseun</h2>
+                  <p className="text-sm text-blue-600">Member</p>
+                </div>
+              </div>
+              <div className="flex space-x-2 text-gray-500">
+                <FontAwesomeIcon
+                  icon={faPencilAlt}
+                  className="size-4 text-blue-900 hover:text-blue-700 cursor-pointer"
+                />
+                <FontAwesomeIcon
+                  icon={faTrashAlt}
+                  className="size-4 text-red-600 hover:text-red-400 cursor-pointer"
+                />
+              </div>
+            </div>
+            <div className="flex space-x-2 mb-2">
+              <span className="bg-blue-800 text-white text-xs px-2 py-0.5 rounded-full">
+                active
+              </span>
+              <span className="bg-gray-200 text-gray-700 text-xs px-2 py-0.5 rounded-full">
+                Prod Design
+              </span>
+            </div>
+            <p className="text-sm text-gray-600 mb-4">
+              Designs visually appealing products that brand your unique identity.
+            </p>
+            <hr className="my-4 border-gray-300" />
+            <div className="text-sm text-gray-600 space-y-1">
+                <p className="flex gap-1.5 items-center hover:underline">
+                <a
+                  href="https://mail.google.com/mail/?view=cm&fs=1&to=Ajibadeoluwaseun018@gmail.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex gap-1.5 items-center"
+                >
+                  <FontAwesomeIcon icon={faEnvelope} className="size-4 text-blue-900" />
+                  Ajibadeoluwaseun018@gmail.com
+                </a>
+              </p>
+              <p className="flex gap-1.5 items-center">
+                <FontAwesomeIcon
+                  icon={faWhatsapp}
+                  className="size-4 text-blue-900"
+                />{" "}
+                (+234) 90234 82470
+              </p>
+              <Link href="https://x.com/kaptal_alex?t=gFaKBZpDgfBcqc1F_ftw2A&s=08">
+                <p className="flex gap-1.5 items-center hover:underline ">
+                  <FontAwesomeIcon
+                    icon={faTwitter}
+                    className="size-4 text-blue-900" />
+                  @kaptal_alex
+                </p>
+              </Link>
+            </div>
+          </div>
+
+        </div>
+
+
+
+
+
+
+
       </div>
+    </div>
     </section>
   );
 }
+
+
+
+
+
+
